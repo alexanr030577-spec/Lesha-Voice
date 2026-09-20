d223116036b8fe987e5ca516213507808f7ea08d
alexanr030577-spec <alexanr030577@gmail.com>
2026-09-20 15:15:49 +0300

    Initial Android voice listener

---
+++ app/build.gradle.kts
@@ -0,0 +1,6 @@
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+android {
+    namespace = "ru.lesha.voice"
+    compileSdk = 35
+    defaultConfig { applicationId = "ru.lesha.voice"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "0.1" }
+}
